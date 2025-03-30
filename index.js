66330aea6d82dae4c1440bb617eea05f4f80a710
Francis Salles <fjsalles.04@gmail.com>
2025-03-31 01:39:21 +0800

    Moved connectMongo to index

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./src/routes/routes');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080
+const uri = process.env.MONGO;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-const connectMongoose = require("./src/connectMongo")
-connectMongoose(() => {
-  app.use('/api/', userRoutes);
-  
-  app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-  });
+const clientOptions = {
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true
+  }
+};
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
+
+app.use('/api/', userRoutes);
+const connectMongoose = async () => {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+connectMongoose()
