ea15a07452edf328e46895c0ed3ef208820d6e6b
Francis Salles <fjsalles.04@gmail.com>
2025-04-07 20:12:19 +0800

    Added content on '/' route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,12 @@ const usersController = require('../controllers/users.controller');
 const termsController = require('../controllers/terms.controller')
 const progressController = require('../controllers/progress.controller')
 
+router.get('/', (req, res) => res.send(`
+  <title>NU Mindify API</title>
+  <h1>NU Mindify API</h1>
+  <div>Example Usage: <a href="https://nu-mindify-api.vercel.app/api/getTerms">https://nu-mindify-api.vercel.app/api/getTerms</a></div>
+  <style>*{text-align:center}</style>
+  `))
 
 router.get('/getUsers', usersController.getUsers);
 router.get('/getUser/:uid', usersController.getUser);
